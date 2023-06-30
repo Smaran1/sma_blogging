0a5b5129b965afe9e79b6cdbd4326566ff9e2950
Smaran <smaran@Smarans-MacBook-Pro.local>
2023-06-30 23:40:36 +0545

    adding model

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.conf import settings
+from django.utils import timezone
 # Create your models here.
+
+class Post(models.Model):
+    author = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+    comments = models.TextField()
+
+
