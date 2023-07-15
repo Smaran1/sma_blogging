0abe201c20913ca6c15ff910f2b9115c0b9cba9a
Smaran <smaran@Smarans-MacBook-Pro.local>
2023-07-15 22:33:42 +0545

    idk man

--- blogs/views.py
+++ blogs/views.py
@@ -35,3 +35,11 @@ def get_all_posts(request):
     post=Post.objects.all()
     serializer= PostSerializer(post, many=True)
     return Response(serializer.data)
+
+@api_view(['POST'])
+def postBlog(request):
+    serializer = PostSerializer(data=request.data)
+    print(serializer)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kpk7z$n$n5sw9sy@=^163wr8ta(w*7m!h^m*nhggb8i2-!m#l+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['http://127.0.0.1:8000/']
 
 
 # Application definition
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
-    
+
 }
 
 MIDDLEWARE = [
